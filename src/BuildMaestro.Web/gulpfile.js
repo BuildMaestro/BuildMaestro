23a33f1a61b9e819950d3cc792a84f4668657f43
Martijn van der Corput <mvdcorput@live.nl>
2016-07-29 00:08:18 +0200

    Gulp fix

--- src/BuildMaestro.Web/gulpfile.js
+++ src/BuildMaestro.Web/gulpfile.js
@@ -1,4 +1,5 @@
-﻿/*
+﻿/// <binding BeforeBuild='copy-libs' />
+/*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
@@ -8,10 +9,7 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 // 
 var gulp = require('gulp');
 
-
-gulp.task('default', function () {
-    // place code for your default task here
-});
+gulp.task('default', ['copy-libs']);
 
 gulp.task('copy-libs', function () {
     // Copy/Populate libs folder from dependencies
